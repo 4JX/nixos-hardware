d27ffc350e799e3124afd1dc610b833ec9b9a690
4JX <79868816+4JX@users.noreply.github.com>
2023-11-24 22:29:34 +0100

    Fix breakage

--- lenovo/legion/16ach6h/edid/default.nix
+++ lenovo/legion/16ach6h/edid/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 let
   # This file was obtained from the display while "DDG" mode was enabled.
@@ -11,5 +11,5 @@ in
   hardware.firmware = [ chip_edid ];
 
   boot.kernelParams = [ "drm.edid_firmware=edid/16ach6h.bin" ];
-  boot.initrd.extraFiles."lib/firmware/edid/16ach6h.bin".source = pkgs.runCommandLocal "chip_edid" { } "cp ${./16ach6h.bin} $out";
+  # boot.initrd.extraFiles."lib/firmware/edid/16ach6h.bin".source = pkgs.runCommandLocal "chip_edid" { } "cp ${./16ach6h.bin} $out";
 }
